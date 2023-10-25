6769d705ed39bc28de170e79dfc7a917ce3a6c7a
rbasehewa <rbasehewa@gmail.com>
2023-10-26 00:40:27 +1100

    fixed experience section

--- PortReact/src/App.jsx
+++ PortReact/src/App.jsx
@@ -1,6 +1,7 @@
 
 import styles from './App.module.css';
 import { About } from './componets/About/About';
+import {Experience} from './componets/Experience/Experience';
 import {Hero} from './componets/Hero/Hero';
 import {Navbar} from './componets/Navbar/Navbar';
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   );
 }
